2fd50756444cb0521b56bdfebd2c54001a31199c
Stanley <2001stany@gmail.com>
2020-12-22 16:48:21 +0300

    class of order details created

--- send_email_app/models.py
+++ send_email_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ClientOrder(models.Model):
+    name = models.CharField(max_length=20)
+    phone_number = models.IntegerField()
+    location = models.CharField(max_length=20)
+    price = models.IntegerField()
+    email = models.EmailField()
+    description = models.TextField(blank=True, null=True)
+    # order_name = models.CharField(max_length=20)
