993a9b32346852e9835b3e287054edda2ffd4f6e
Stanley <2001stany@gmail.com>
2021-01-05 07:53:35 +0300

    env variable issue fixed

--- NIFULIE_AGENT_API/settings.py
+++ NIFULIE_AGENT_API/settings.py
@@ -1,9 +1,7 @@
 
 from pathlib import Path
 import os
-
-
-
+from decouple import config
 
 
 
@@ -15,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- send.py
+++ send.py
@@ -1,17 +1,3 @@
-from django.core.mail import EmailMessage
+# from django.core.mail import EmailMessage
 
 # import os
-# # send_mail('hello maen', 'this is a message','2001stany@gmail.com', ['doccasheby@gmail.com',])
-
-
-# print(os.environ.get('EMAIL_HOST_USER'))
-
-try:
-    email = EmailMessage(subject='hi', body='testing mail',
-        from_email='2001stany@gmail.com', to=['doccasheby@gmail.com',])
-    
-    email.send()
-    print('success')
-
-except:
-    print('failed')
