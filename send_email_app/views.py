f8d9b3bf88d36ab95b30ca70bc47523d8e2f1459
Stanley <2001stany@gmail.com>
2021-01-05 07:03:28 +0300

    rest framework set

--- send_email_app/views.py
+++ send_email_app/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from serializers import RegisterSerilizer
 
+
+
+class RegisterViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerilizer
